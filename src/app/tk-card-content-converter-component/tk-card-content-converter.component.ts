df02d946f900f5ef84c2dd88d89f036b9a83940c
fernando <fernando.albsilva@gmail.com>
2021-10-09 22:09:24 -0300

    incluindo expressoes regulares no conversor de base

--- src/app/tk-card-content-converter-component/tk-card-content-converter.component.ts
+++ src/app/tk-card-content-converter-component/tk-card-content-converter.component.ts
@@ -28,41 +28,49 @@ export class CardContentConverterComponent {
       case 'inBinary':
         this.inBinary = !this.inBinary;
         this.clearOtherCheckBoxes('inBinary');
+        this.checkChars("input");
         break;
 
       case 'inOctal':
         this.inOctal = !this.inOctal;
         this.clearOtherCheckBoxes('inOctal');
+        this.checkChars("input");
         break;
 
       case 'inDecimal':
         this.inDecimal = !this.inDecimal;
         this.clearOtherCheckBoxes('inDecimal');
+        this.checkChars("input");
         break;
 
       case 'inHex':
         this.inHex = !this.inHex;
         this.clearOtherCheckBoxes('inHex');
+        this.checkChars("input");
         break;
 
       case 'outBinary':
         this.outBinary = !this.outBinary;
         this.clearOtherCheckBoxes('outBinary');
+        this.checkChars("output");
         break;
 
       case 'outOctal':
         this.outOctal = !this.outOctal;
         this.clearOtherCheckBoxes('outOctal');
+        this.checkChars("output");
         break;
 
       case 'outDecimal':
         this.outDecimal = !this.outDecimal;
         this.clearOtherCheckBoxes('outDecimal');
+        this.checkChars("output");
         break;
 
       case 'outHex':
         this.outHex = !this.outHex;
         this.clearOtherCheckBoxes('outHex');
+        this.checkChars("output");
         break;
 
       default:
@@ -154,34 +162,61 @@ export class CardContentConverterComponent {
     }
   }
 
-  public checkChars(){
-    //TODO
-    // console.log("chamou")
-    // let data;
-    // if(this.inBinary){
-    //   let rgx = /^[0-1]{1,}$/;
-    //   data = this.inputData.match(rgx);
-    //   if(data !== null) { this.inputData = data.toString()}
-    //   else { this.inputData = this.inputData.slice(0, -1);}
-    // }
-    // if(this.inOctal){
-    //   let rgx = /^[0-7]{1,}$/;
-    //   data = this.inputData.match(rgx);
-    //   if(data !== null) { this.inputData = data.toString()}
-    //   else { this.inputData = this.inputData.slice(0, -1);}
-    // }
-    // if(this.inDecimal){
-    //   let rgx = /^[0-9]{1,}$/;
-    //   data = this.inputData.match(rgx);
-    //   if(data !== null) { this.inputData = data.toString()}
-    //   else { this.inputData = this.inputData.slice(0, -1);}
-    // }
-    // if(this.inHex){
-    //   let rgx = /\b[0-9A-F]+\b/;
-    //   data = this.inputData.match(rgx);
-    //   if(data !== null) { this.inputData = data.toString()}
-    //   else { this.inputData = this.inputData.slice(0, -1);}
-    // }
+  public checkChars(typeOfEvent:string){
+    console.log("chamou")
+    let data;
+    const rgxBinary = /0|1/g;
+    const rgxOctal = /0|1|2|3|4|5|6|7/g;
+    const rgxDecimal = /0|1|2|3|4|5|6|7|8|9/g;
+    const rgxHex = /0|1|2|3|4|5|6|7|8|9|a|b|c|d|e|f/gi;
+
+    if(typeOfEvent === "input")
+    {
+      if(this.inBinary){
+        data  = this.inputData.match(rgxBinary)?.join('');
+        if(!!data) { this.inputData = data}
+        else{  this.inputData = "";}
+      }
+      if(this.inOctal){
+        data  = this.inputData.match(rgxOctal)?.join('');
+        if(!!data) { this.inputData = data}
+        else{  this.inputData = "";}
+      }
+      if(this.inDecimal){
+        data  = this.inputData.match(rgxDecimal)?.join('');
+        if(!!data) { this.inputData = data}
+        else{  this.inputData = "";}
+      }
+      if(this.inHex){
+        data  = this.inputData.match(rgxHex)?.join('');
+        if(!!data) { this.inputData = data}
+        else{  this.inputData = "";}
+      }
+    }
+    if(typeOfEvent === "output")
+    {
+      if(this.outBinary){
+        data  = this.outputData.match(rgxBinary)?.join('');
+        if(!!data) { this.outputData = data}
+        else{  this.outputData = "";}
+      }
+      if(this.outOctal){
+        data  = this.outputData.match(rgxOctal)?.join('');
+        if(!!data) { this.outputData = data}
+        else{  this.outputData = "";}
+      }
+      if(this.outDecimal){
+        data  = this.outputData.match(rgxDecimal)?.join('');
+        if(!!data) { this.outputData = data}
+        else{  this.outputData = "";}
+      }
+      if(this.outHex){
+        data  = this.outputData.match(rgxHex)?.join('');
+        if(!!data) { this.outputData = data}
+        else{  this.outputData = "";}
+      }
+
+    }
   }
 
   private convertToDecimal(data:any){
